78bdab8d0975f5958025a8e21d147650f1d1cb93
imam0321 <imam.hossain0321@gmail.com>
2025-06-24 04:08:16 +0600

    fix: Correct color format in CSS variables and comment out unused import in Profile component

--- src/pages/HomePage/Profile/Profile.jsx
+++ src/pages/HomePage/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import profile from "../../../assets/profile.jpg"
 import SocialLinks from "../../../components/SocialLinks";
 
